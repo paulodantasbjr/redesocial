96630ab48e05f6ef4aec1acee0fc896560515201
Paulo Dantas <paulo.dantas.solutic@petrobras.com.br>
2021-01-29 09:59:15 -0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <main className='flex flex-col items-center justify-center flex-1 px-20 text-center'>
+      <h1 className='text-6xl font-bold'>Welcome to RedeSocial</h1>
+    </main>
+  )
+}
+
+export default index
